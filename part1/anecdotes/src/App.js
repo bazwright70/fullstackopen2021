dff9acb94adb5ec418d5008621a1e8a92ee93273
Brian Wright <bazwright1970@gmail.com>
2021-06-27 12:20:19 +0100

    unicafe 1.14 added display of most votes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -15,6 +15,7 @@ const App = () => {
 
   const [votes, setVotes] = useState(arr);
   const [selected, setSelected] = useState(0);
+  // const [topAnecdote, setTopAnecdote] = useState(0)
 
   let rand;
 
@@ -29,18 +30,32 @@ const App = () => {
     setVotes(newVotes);
   }
 
+  const setTop = () => {
+    const top = votes.indexOf(Math.max(...votes));
+    if (votes.some(el => el > 0)) {
+      return `"${anecdotes[top]}" has ${votes[top]} votes `;
+    } else {
+      return "No one has voted yet"
+    }
+  }
+
 
   return (
-    <div>
+    <>
+      <p><b>Anecdote of the day</b></p>
       <p>
         {anecdotes[selected]}
       </p>
       <p>
         has {votes[selected]} votes.
       </p>
+
       <button onClick={updateVote}>Vote</button>
       <button onClick={selectAnecdote}>Next Anecdote</button>
-    </div>
+
+      <p><b>Anecdote with the most votes</b></p>
+      <p> {setTop()} </p>
+    </>
   );
 }
 
