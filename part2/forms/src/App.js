570406444b87278e4bf3cf97ce9f89d0a3a8d959
Brian Wright <bazwright1970@gmail.com>
2021-06-29 12:29:39 +0100

    part 2 forms - added create note form functionality

--- part2/forms/src/App.js
+++ part2/forms/src/App.js
@@ -4,6 +4,28 @@ import Note from './components/Note'
 const App = (props) => {
 
   const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('');
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const addNote = createNote();
+    setNotes(notes.concat(addNote));
+    setNewNote('');
+  }
+
+  const createNote = () => {
+    const noteObject = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+    }
+    return noteObject;
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  }
 
   return (
     <div>
@@ -13,6 +35,14 @@ const App = (props) => {
           return <Note key={note.id} note={note.content} />
         })}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          placeholder="add new note..."
+          onChange={handleNoteChange}
+        />
+        <button type="submit">Save</button>
+      </form>
     </div>
   );
 }
