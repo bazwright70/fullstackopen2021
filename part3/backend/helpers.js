a632383eb3b9ceb21f7f7edc770358eaf723d622
Brian Wright <bazwright1970@gmail.com>
2021-07-13 11:13:56 +0100

    resolved typo in unsaved expoert after last commit

---
+++ part3/backend/helpers.js
@@ -0,0 +1,8 @@
+module.exports = {
+  generateId: function(){
+    
+    const maxId = notes.length > 0 ?
+    Math.max(...notes.map(n => n.id)) : 0
+    return maxId + 1;
+  }
+}
---
+++ part3/backend/index.js
@@ -0,0 +1,61 @@
+const { response, request } = require('express');
+const express = require('express');
+const app = express();
+let notes = require('./notes');
+const { generateId } = require('./helpers');
+
+// middleware
+app.use(express.json())
+
+// GET root route
+app.get('/',(request, response) => {
+  response.send('<h1>Hello World</h1>')
+});
+
+// GET all notes
+app.get('/api/notes', (request, response)=>{
+  response.send(notes)
+});
+// CREATE new note
+app.post('/api/notes',(request, response)=>{
+  const body = request.body;
+  if(!body.content){
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }    
+  const note = {
+    content: body.content,
+    id: generateId(),
+    important: body.important || false,
+    date: new Date(),   
+  };
+
+  notes = notes.concat(note)
+  console.log(notes);
+  response.json(note);
+})
+
+// GET single note
+app.get('/api/notes/:id',(request, response)=>{
+  const id = Number(request.params.id);
+  const note = notes.find( n => n.id === id);
+  if(note){
+    response.json(note)
+  }else{
+    response.status(404).end();
+  }  
+});
+
+// DELETE single note
+app.delete('/api/notes/:id',(request, response)=>{
+  const id = Number(request.params.id);
+    notes = notes.filter( note =>{
+    return note.id !== id;
+  });
+  response.status(204).end();
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
