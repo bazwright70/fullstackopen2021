3d8be2d3c58df34a9a0c9cd1d75e67c91431e2a2
Brian Wright <bazwright1970@gmail.com>
2021-07-17 11:02:14 +0100

    3.12 phonebook-final  - added argv check for either adding new contact or displaying all contacts

--- part2/phonebook/mongo.js
+++ part2/phonebook/mongo.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose')
 const db = require('./db.json')
 // check if password is supplied in node args
 if(process.argv.length < 3){
-  console.log('Please provide the mongo password as an argument to node');
+  console.log('Please provide Mongo a password to connect to the database');
   process.exit(1)
 }
 
-const password = process.argv[2];
+
+const [,, password, name, number] = process.argv;
+const newContact = {
+  name,number
+}
 
 // Monog Atlas connection parameters
 const url = `mongodb+srv://fullstack:${password}@fullstackopen2021.8fbct.mongodb.net/phonebook?retryWrites=true&w=majority`
@@ -19,9 +23,9 @@ const options = {
 
 // connect to mongo Atlas
 mongoose.connect(url, options)
-.then(console.log('Connection to Mongo accepted'))
+.then(console.log('*** LOG: Connection to Mongo accepted ***'))
 .catch(err => {
-  console.log("Mongo Connection Error: ", err)
+  console.log("*** LOG: Mongo Connection Error: ", err)
 });
 
 // create Mongo schema
@@ -32,30 +36,34 @@ const contactSchema = new mongoose.Schema({
 // create Mongo Model
 const Contact = mongoose.model('contact', contactSchema);
 
-// test db file 
-//console.log(db)
-
-// bulk create contact documents
-// Contact.create(db.persons)
-// .then(result => console.log("documents created"))
-// .catch(err => console.log("Error creating documents", err))
-// .finally(result => {
-//   mongoose.connection.close();
-//   console.log('connection closed');
-// });
-
+if(process.argv.length === 5){
+  Contact.create(newContact)
+  .then(result => {
+    console.log(`added ${name} ${number} to phonebook`)
+  })
+  .catch(err => console.log("*** LOG: Error adding record", err))
+  .finally(()=>{
+    mongoose.connection.close();
+    console.log('*** LOG: Mongo connection closed ***')
+  })
+}else{
 //retrieve all db contacts
 Contact.find({})
   .then(contacts => {
+    console.log("Phonebook:")
     contacts.forEach(contact=>{
-      console.log(contact)
+      console.log(`${contact.name} ${contact.number}`)
     })
   })
-  .catch(err => console.log("Error retrieving documents", err))
+  .catch(err => console.log("*** LOG: Error retrieving documents", err))
   .finally(result => {
       mongoose.connection.close();
-      console.log('connection closed');
+      console.log('*** LOG: connection closed ***');
     });
+}
+
+
+
   
 
 
