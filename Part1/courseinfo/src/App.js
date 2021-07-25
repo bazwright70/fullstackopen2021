f06fd411b394e6c07e72521d0ff2e69d10c867ed
Brian Wright <bazwright1970@gmail.com>
2021-07-25 14:56:06 +0100

    1.2 courseinfo added separate components

--- Part1/courseinfo/src/App.js
+++ Part1/courseinfo/src/App.js
@@ -1,21 +1,55 @@
 import React from 'react';
 
-function App() {
-  const course = 'Half Stack application development'
+const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  const  total = exercises1 + exercises2 + exercises3;
+
+const Header = ({courseName}) => {
+  return(
+    <div>
+      <h1>{courseName}</h1>
+    </div>
+  )
+}
+
+const Part = ({part, exercise}) => {  
+  return(
+    <div>
+      <p>{part} {exercise}</p>
+    </div>
+  )
+}
+
+const Total  = ({total}) => {
+  return(
+    <div>
+      <h1>Number of exercises is {total}</h1>
+    </div>
+  )
+}
+
+const Content = () => {
+  return(
+    <div>
+      <Part part={part1} exercise={exercises1}/>
+      <Part part={part2} exercise={exercises2}/>
+      <Part part={part3} exercise={exercises3}/>
+    </div>
+  )
+}
+
+function App() {
 
   return (
     <div>
-      <h1>{course}</h1> 
-      <p>{part1} {exercises1}</p>
-      <p>{part2} {exercises2}</p>
-      <p>{part3} {exercises3}</p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header courseName={course}/>
+      <Content />
+      <Total total={total}/>
     </div>
   );
 }
---
+++ Part1/courseinfo/src/Total.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Total  = ({total}) => {
+  return(
+    <div>
+      <h1>Number of exercises is {total}</h1>
+    </div>
+  )
+}
+
+export default Total;
