7bb32645cfb077a10438cc4d99f9f60ae7956bba
Brian Wright <bazwright1970@gmail.com>
2021-07-26 12:19:22 +0100

    1.6 - unicafe - create app. Added changes that didn't coomit previously

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from "react";
 
-function App() {
+//  button component
+const Button = ({label, handler}) => {
+  return(
+    <button onClick={handler}>{label}</button>
+  )
+}
+// indiv stat component
+const Stat = ({label, val}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p>{label} {val}</p>
+  )
+}
+
+// App component
+function App() {
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleClick = (stat) => {
+    switch(stat){
+      case 'good': setGood(good +1);
+                  break;
+      case 'neutral': setNeutral(neutral +1);
+                  break;
+      case 'bad': setBad(bad +1);
+                  break;
+    }
+  }
+    return (
+    <div >
+      <h2>Give Your Feedback</h2>
+        <Button handler={()=>handleClick('good')} label=' Good' />
+        <Button handler={()=>handleClick('neutral')} label=' Neutral' />
+        <Button handler={()=>handleClick('bad')} label=' Bad' />
+      <h2>Statistics</h2>
+        <Stat label='Good' val={good}/>
+        <Stat label='Neutral' val={neutral}/>
+        <Stat label='Bad' val={bad}/>
     </div>
   );
 }
