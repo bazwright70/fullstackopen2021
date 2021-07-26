9ef61749a010f80913b83fa5c6cd03c995d39aa9
Brian Wright <bazwright1970@gmail.com>
2021-07-26 11:52:01 +0100

    final code for study section

--- Part1/part1/src/App.js
+++ Part1/part1/src/App.js
@@ -1,18 +1,43 @@
 import React, { useState } from 'react';
 
-const App = () =>  {
-  const [counter, setCounter ] = useState(0);
+const History = (props) => {
+  if(props.allClicks.length===0){
+    return(
+      <div>Click History will appear here</div>
+    )}else{
+      return (
+        <div>Button History:- {props.allClicks.join('  ')}</div>
+      )
+    }  
+}
 
-  const handleClick = () =>{
-    console.log('Clicked..')
-    setCounter(counter + 1)
+const Button = ({ handler, text }) => {
+  return(
+    <button onClick={handler}>{text}</button>
+  )
+}
+
+const App = () =>  {
+  const [left, setLeft ] = useState(0);
+  const [right, setRight ] = useState(0);
+  const [ allClicks, setAll] =useState([])
+
+  const handleLeftClick = () => {
+    setLeft(left+1);
+    setAll([allClicks.concat('L')])
+  }
+  const handleRightClick = () => {
+    setRight(right+1);
+    setAll([allClicks.concat('R')])
   }
 
-  console.log('Rendering ...')
     return (
     <div>
-        {counter}
-        <button onClick = {handleClick}>Click Me</button>
+        {left}
+        <Button handler={ handleLeftClick } text='Left' />
+        <Button  handler={ handleRightClick } text='Right' />
+        {right}
+        <History allClicks={allClicks}/>
     </div>
     
   );
