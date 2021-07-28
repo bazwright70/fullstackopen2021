f6cdc5aa17c4db15e376da4b10da2280facb381d
Brian Wright <bazwright1970@gmail.com>
2021-07-28 11:19:05 +0100

    2.7 phonebook - add ability to add and display a new name and number

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -2,24 +2,28 @@ import React,{useState} from 'react';
 
 const App = () => {
 
-  const [newName,setNewName] = useState('')
+  const [newName,setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
   const [persons, setPersons] = useState([
-    {name:'Arto Hellas'}
+    {name:'Arto Hellas',number: '04565235658'}
   ])
 
   const handleForm = (event) => {
     event.preventDefault();
-    
-    if(persons.some(person => person.name.toLowerCase() === newName.toLowerCase()))
-      {
-      alert('${newName} is already in the phonebook');
+    const isDupe = persons.some(person => {
+      return person.name.toLowerCase() === newName.toLowerCase()
+    })
+    if(isDupe){
+        alert(`${newName} is already in the phonebook`);
       }else{
         const personObj = {
-          name: newName
+          name: newName,
+          number: newNumber
         }
       setPersons(persons.concat(personObj));     
     }
     setNewName('');
+    setNewNumber('');
   }
 
   return (
@@ -27,18 +31,30 @@ const App = () => {
       <div>debug: {newName}</div>
       <h2>Phonebook</h2>
       <form onSubmit={handleForm}>
-        Name:
-        <input 
-          type="text"
-          placeholder="Add new name..."
-          value={newName}
-          onChange={(e)=>{setNewName(e.target.value)}}
-        />
+        <div>
+          Name: <input 
+            type="text"
+            placeholder="Add new name..."
+            value={newName}
+            onChange={(e)=>{setNewName(e.target.value)}}
+          />
+        </div>
+        <div>
+          Number:<input 
+            type="text"
+            placeholder="Add new number..."
+            value={newNumber}
+            onChange={(e)=>{setNewNumber(e.target.value)}}
+          />
+        </div>
+       
         <button type="submit">Add</button>
       </form>
       <h2>Numbers</h2>
       <ul>
-      {persons.map(person => <li key={person.name}>{person.name}</li>)}
+      {persons.map(person => {
+        return <li key={person.name}>{person.name} {person.number}</li>
+      })}
       </ul>
     </div>
   )
