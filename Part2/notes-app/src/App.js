e78e48f6793e201225175cbe908061b4add2e973
Brian Wright <bazwright1970@gmail.com>
2021-07-28 10:02:19 +0100

    notes-app add button toggle for important notes

--- Part2/notes-app/src/App.js
+++ Part2/notes-app/src/App.js
@@ -2,15 +2,52 @@ import React, {useState} from 'react';
 import Note from './components/Note.js'
 
 
-const App = ({ notes }) =>  {
+const App = (props) =>  {
+
+const [notes, setNotes] = useState(props.notes);
+const [newNote, setNewNote] = useState('');
+const [showAll, setShowAll] = useState(true)
+
+const addNote = (event) => {
+  event.preventDefault();
+  const noteObject = {
+    id: notes.length + 1,
+    date: new Date().toISOString(),
+    content: newNote,
+    important: Math.random() > 0.499999
+  }
+  setNotes(notes.concat(noteObject));
+  setNewNote('');
+}
+
+const handleNoteChange = (event)=>{
+  setNewNote(event.target.value)
+}
+
+const notesToShow = showAll 
+  ? notes
+  : notes.filter( note => note.important);
+
   return (
     <div >
       <h1>Notes</h1>
+      <button onClick={()=>{setShowAll(!showAll)}}>
+        {showAll ? "Show Important" : "Show All"}
+      </button>
       <ul>
-        {notes.map( note => 
+        {notesToShow.map( note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input 
+          type="text" 
+          value={newNote} 
+          onChange={handleNoteChange}
+          placeholder="Add new note..."
+        />
+        <button type="submit">Save</button>
+      </form>
     </div>
   );
 }
