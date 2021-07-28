45376347abad54fe20d2f5c2672d5aaf59e66747
Brian Wright <bazwright1970@gmail.com>
2021-07-28 10:29:47 +0100

    2.6 - phonebook - create basic app to add new names to phonebook from form input and button

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const App = () => {
+
+  const [newName,setNewName] = useState('')
+  const [persons, setPersons] = useState([
+    {name:'Arto Hellas'}
+  ])
+
+  const handleForm = (event) => {
+    event.preventDefault();
+    const personObj = {
+      name: newName
+    }
+    setPersons(persons.concat(personObj))
+    setNewName('');
+  }
+
   return (
-    <div>App component</div>
+    <div>
+      <div>debug: {newName}</div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleForm}>
+        Name:
+        <input 
+          type="text"
+          placeholder="Add new name..."
+          value={newName}
+          onChange={(e)=>{setNewName(e.target.value)}}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+      {persons.map(person => <li key={person.name}>{person.name}</li>)}
+      </ul>
+    </div>
   )
 }
 
