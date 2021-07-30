a5e72bde93cfee02d2f76085f8aa0c532b03a26f
Brian Wright <bazwright1970@gmail.com>
2021-07-30 09:48:01 +0100

    2.12 broken changes

--- Part2/countries/src/App.js
+++ Part2/countries/src/App.js
@@ -1,12 +1,9 @@
 import React, {useState, useEffect } from 'react';
 import axios from 'axios';
-import Display from './Display.js';
-
 
 const App = () => {
   const [countries, setCountries ] = useState([]);
   const [ filter, setFilter ] = useState('');
-  const [ready,setReady] = useState(false);
 
   const url = ('https://restcountries.eu/rest/v2/all');
 
@@ -15,11 +12,35 @@ const App = () => {
       .get(url)
       .then(res => {
           setCountries(res.data);
-          setReady(true)
       })
       .catch(err => console.log("**LOG -ERROR: ", err))
   },[])
 
+const filterResults = (countries, filterVal) => {
+  const results =  countries.filter(country =>{
+    return country.name.toLowerCase().includes(filter.toLowerCase())
+  });
+  return results.length > 11 ?
+    [{name: 'Too many results, try a better search',
+    alpha3Code: 'tooLong'  
+    }] :
+    results.length > 1 ? results :
+    createCountry(results)
+}
+
+const createCountry = (result) => {
+  if(result.length){
+   const {
+     name, capital,population, languages,flag
+   } = result[0]
+    return [
+      {
+        name, capital,population, languages,flag
+      }
+    ]
+  }
+  
+}
   return (
     <div >
       Find Countries:
@@ -29,13 +50,9 @@ const App = () => {
         onChange={(e)=>{setFilter(e.target.value)}} />
         <ul>
       { 
-         countries.filter(country =>{
-          return country.name.toLowerCase().includes(filter.toLowerCase())
-        }).length > 10 ?
+         filterResults(countries, filter).length > 10 ?
          (<p> too many matches, try a better filter</p>) :
-         countries.filter(country =>{
-          return country.name.toLowerCase().includes(filter.toLowerCase())
-        }).map( country=>{
+         filterResults(countries, filter).map( country=>{
           return <li key={country.alpha3Code}>
               {country.name}
           </li>
